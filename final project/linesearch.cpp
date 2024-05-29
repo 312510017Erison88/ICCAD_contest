40672b09f4c5a9a6b9cbc7a2bf2028f53bd253b9
312510017Erison88 <songerison@gmail.com>
2024-05-30 00:48:40 +0800

    init linesearch.cpp and .h file

---
+++ final project/linesearch.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <vector>
+#include "linesearch.h"
+
+using namespace std;
+
---
+++ final project/linesearch.h
@@ -0,0 +1,11 @@
+#ifndef _LINESEARCH_H_
+#define _LINESEARCH_H_
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+
+#endif
